0251b860a14229c7fa92d2cd6f909f32928dbdb3
giwastech1 <giwatoheeb27@gmail.com>
2026-08-02 13:15:17 +0100

    show options in the menu

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿DisplayMenu();
+ChooseOption();
 //function to display the menu.
 static void DisplayMenu()
 {
     Console.WriteLine("==========================\nCALCULATOR\n==========================");
 }
 
+//function to choose options of program
+static void ChooseOption()
+{
+    Console.WriteLine("What kind of operation will you like to perform ?");
+    Console.WriteLine("1. Addittion");
+    Console.WriteLine("2. Substraction");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Exit");
+}

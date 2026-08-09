f010c1e02800d3bf4915fe808fc07ca1b754ddfb
giwastech1 <giwatoheeb27@gmail.com>
2026-08-09 13:48:25 +0100

    fix invalid input from reading from console.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿//DisplayMenu();
-//function to display the menu.
+﻿using System.IO.Pipelines;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
 bool running = true;
 static void DisplayMenu()
     {
@@ -14,57 +16,87 @@ static void DisplayMenu()
 while (running)
 {
     DisplayMenu();
-    
-//ask user to input their desire option
-Console.Write("Choose an option: ");
-int option = Convert.ToInt32(Console.ReadLine());
-switch (option)
-{
+
+    //ask user to input their desire option
+    Console.Write("Choose an option: ");
+    //int option = Convert.ToInt32(Console.ReadLine());
+    string? option = Console.ReadLine();
+    if (int.TryParse(option, out int validInput))
+    {
+        validInput = Convert.ToInt32(option);
+    }
+    else
+    {
+        Console.WriteLine("Enter a valid input.");
+    }
+    switch (validInput)
+    {
         case 1:
             int firstInput = ReadNumber("Enter your first input: ");
             int secondInput = ReadNumber("Enter your second input: ");
             int output = Addition(firstInput, secondInput);
-           Console.WriteLine($"The result of {firstInput} + {secondInput} is {output}");
             Console.WriteLine();
-        break;
-    case 2:
-        firstInput = ReadNumber("Enter your first input: ");
-        secondInput = ReadNumber("Enter your second input: ");
-        output = Substraction(firstInput, secondInput);
-        Console.WriteLine($"The result of {firstInput} - {secondInput} is {output}");
-        break;
-    case 3:
-        firstInput = ReadNumber("Enter your first input: ");
-        secondInput = ReadNumber("Enter your second input: ");
-        output = Multiplication(firstInput, secondInput);
-        Console.WriteLine($"The result of {firstInput} x {secondInput} is {output}");
-        break;
-    case 4:
-        firstInput = ReadNumber("Enter your first input: ");
-        secondInput = ReadNumber("Enter your second input: ");
-        if (secondInput == 0)
-        {
+            Console.WriteLine($"The result of {firstInput} + {secondInput} is {output}");
+            Console.WriteLine();
+            break;
+        case 2:
+            firstInput = ReadNumber("Enter your first input: ");
+            secondInput = ReadNumber("Enter your second input: ");
+            output = Substraction(firstInput, secondInput);
+            Console.WriteLine();
+            Console.WriteLine($"The result of {firstInput} - {secondInput} is {output}");
+            break;
+        case 3:
+            firstInput = ReadNumber("Enter your first input: ");
+            secondInput = ReadNumber("Enter your second input: ");
+            output = Multiplication(firstInput, secondInput);
+            Console.WriteLine();
+            Console.WriteLine($"The result of {firstInput} x {secondInput} is {output}");
+            Console.WriteLine();
+            break;
+        case 4:
+            firstInput = ReadNumber("Enter your first input: ");
+            secondInput = ReadNumber("Enter your second input: ");
+            if (secondInput == 0)
+            {
                 Console.WriteLine("You cannot divide by zero");
                 return;
-        }
-        output = Division(firstInput, secondInput);
-        Console.WriteLine($"The result of {firstInput} / {secondInput} is {output}");
+            }
+            output = Division(firstInput, secondInput);
+            Console.WriteLine();
+            Console.WriteLine($"The result of {firstInput} / {secondInput} is {output}");
+            Console.WriteLine();
             break;
         case 5:
             running = false;
             break;
-    default:
-        Console.WriteLine("Wrong input");
-        break;
-}
+        default:
+            Console.WriteLine("option does not exist");
+            Console.WriteLine();
+            break;
+    }
 }
 
 
-//function to read num from console
+//function to read num from console.
 static int ReadNumber(string message)
 {
-    Console.Write($"{message}");
-    return Convert.ToInt32(Console.ReadLine());
+    bool isNotNumber = true;
+    int result = 0;
+    while (isNotNumber)
+    {
+        Console.Write($"{message}");
+        string? readFromConsole = Console.ReadLine();
+        if (int.TryParse(readFromConsole, out result))
+        {
+            isNotNumber = false;
+        }
+        else
+        {
+            Console.WriteLine("Error input, enter a number");
+        }
+    }
+    return result;
 }
 //Addition program
 static int Addition(int a, int b)

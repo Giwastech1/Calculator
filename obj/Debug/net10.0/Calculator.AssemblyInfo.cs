2379cff0367a034ba8fca40a14a41aad80535e6f
giwastech1 <giwatoheeb27@gmail.com>
2026-08-15 23:17:29 +0100

    add gitigonore

--- obj/Debug/net10.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net10.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b25b154934ada8ac4bc155d8c463171831b0f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67de96a545c962a71621851e02f86df1ba2134b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
